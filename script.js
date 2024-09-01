84471f6c495c80e0a67695e7c93c5e43bb3dae72
Vishal27676 <121402398+Vishal27676@users.noreply.github.com>
2024-09-01 15:17:00 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(event){
+    event.preventDefault();
+    alert("Thank you for your message. I'll get back to you soon.");
+    // Add logic here to handle form data
+});
